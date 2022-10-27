0c9bbd7b9478d7102760514adb7dd36291bd03bb
raph2ll <raphael.ph2ll@gmail.com>
2022-10-27 19:53:55 -0300

    feat(api): change PORT

--- Backend/src/api/index.js
+++ Backend/src/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('../router');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
