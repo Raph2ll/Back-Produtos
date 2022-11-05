738ef435f37d91b86eb39ec89595f16076c07c2f
raph2ll <raphael.ph2ll@gmail.com>
2022-11-04 22:11:43 -0300

    refactor(api): split server port

--- Backend/src/api/app.js
+++ Backend/src/api/app.js
@@ -4,13 +4,9 @@ const swaggerUi = require('swagger-ui-express');
 const router = require('../router');
 const swaggerDocument = require('../../swagger.json');
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', router);
-
-app.listen(PORT, () => console.log(`localhost:${PORT}`));
